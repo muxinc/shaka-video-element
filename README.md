9b75e23cf4a402d77a9ee087d41f59c1ccd57631
heff <git@heff.me>
2022-10-12 17:41:20 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# `<shaka-video>`
+
+A custom element (web component) for [Shaka Player](https://github.com/shaka-project/shaka-player), which supports both HLS and DASH streaming protocols.
+
+The element API matches the HTML5 `<video>` tag, so it can be easily swapped with other media, and be compatible with other UI components that work with the video tag, including [media chrome](https://www.media-chrome.org/).
+
+## Example
+
+```html
+<html>
+<head>
+  <script type="module" src="https://unpkg.com/shaka-video-element@0.0"></script>
+</head>
+<body>
+
+  <shaka-video controls src="https://stream.mux.com/DS00Spx1CV902MCtPj5WknGlR102V5HFkDe.m3u8"></shaka-video>
+
+</body>
+</html>
+```
+
+## Installing
+
+`<shaka-video>` is packaged as a javascript module (es6) only, which is supported by all evergreen browsers and Node v12+.
+
+### Loading into your HTML using `<script>`
+
+Note the `type="module"`, that's important.
+
+> Modules are always loaded asynchronously by the browser, so it's ok to load them in the head :thumbsup:, and best for registering web components quickly.
+
+```html
+<head>
+  <script type="module" src="https://unpkg.com/shaka-video-element@0.0"></script>
+</head>
+```
+
+### Adding to your app via `npm`
+
+```bash
+npm install shaka-video-element --save
+```
+
+Include in your app javascript (e.g. src/App.js)
+```js
+import 'shaka-video-element';
+```
+This will register the custom elements with the browser so they can be used as HTML.
---
+++ index.js
@@ -0,0 +1,97 @@
+// Web Components: Extending Native Elements, A working example
+
+import CustomVideoElement from 'custom-video-element';
+import shaka from 'shaka-player';
+
+class ShakaVideoElement extends CustomVideoElement {
+  constructor() {
+    super();
+
+    console.log(shaka);
+  }
+
+  get src() {
+    // Use the attribute value as the source of truth.
+    // No need to store it in two places.
+    // This avoids needing a to read the attribute initially and update the src.
+    return this.getAttribute('src');
+  }
+
+  set src(val) {
+    // If being set by attributeChangedCallback,
+    // dont' cause an infinite loop
+    if (val !== this.src) {
+      this.setAttribute('src', val);
+    }
+  }
+
+  async load() {
+    const player = new shaka.Player(this.nativeEl);
+
+    // Listen for error events.
+    player.addEventListener('error', onErrorEvent);
+
+    // Try to load a manifest.
+    // This is an asynchronous process.
+    try {
+      await player.load(this.src);
+      // This runs if the asynchronous load is successful.
+      console.log('The video has now been loaded!');
+    } catch (e) {
+      // onError is executed if the asynchronous load fails.
+      onError(e);
+    }
+
+    function onErrorEvent(event) {
+      // Extract the shaka.util.Error object from the event.
+      onError(event.detail);
+    }
+    
+    function onError(error) {
+      // Log the error.
+      console.error('Error code', error.code, 'object', error);
+    }
+
+
+    // if (Hls.isSupported()) {
+    //   var hls = new Hls({
+    //     // Kind of like preload metadata, but causes spinner.
+    //     // autoStartLoad: false,
+    //   });
+
+    //   hls.loadSource(this.src);
+    //   hls.attachMedia(this.nativeEl);
+    // } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    //   this.nativeEl.src = this.src;
+    // }
+  }
+
+  // play() {
+  //   if (this.readyState === 0 && this.networkState < 2) {
+  //     this.load();
+  //     this.hls.on(Hls.Events.MANIFEST_PARSED,function() {
+  //     video.play();
+  //
+  //     return this.nativeEl.play();
+  //   }
+  // }
+
+  connectedCallback() {
+    this.load();
+
+    // Not preloading might require faking the play() promise
+    // so that you can call play(), call load() within that
+    // But wait until MANIFEST_PARSED to actually call play()
+    // on the nativeEl.
+    // if (this.preload === 'auto') {
+    //   this.load();
+    // }
+  }
+}
+
+if (!window.customElements.get('shaka-video')) {
+  window.customElements.define('shaka-video', ShakaVideoElement);
+  window.ShakaVideoElement = ShakaVideoElement;
+}
+
+export default ShakaVideoElement;
